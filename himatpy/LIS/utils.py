aef9b7566bddc03833d901f80d838c0360b2a4bc
lsetiawan <landungs@uw.edu>
2017-11-09 16:34:39 -0800

    Add function to get monthly avg

--- himatpy/LIS/utils.py
+++ himatpy/LIS/utils.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+from exceptions import IOError
 
 import numpy as np
 import xarray as xr
+from dask.diagnostics import ProgressBar
 
 __author__ = ['Anthony Arendt', 'Landung Setiawan']
 
@@ -41,3 +43,22 @@ def get_xr_dataset(datadir, fname=None, multiple_nc=False):
     ds.coords['latitude'] = (('north_south', 'east_west'), np.swapaxes(data, 0, 1))
 
     return ds
+
+
+def get_monthly_avg(ds, des_vars, export_nc=False, out_pth=None):
+    new_ds = None
+    for idx, var in enumerate(des_vars):
+        with ProgressBar():
+            da = ds[var].resample('MS', 'time', how = 'sum')
+        if idx == 0:
+            new_ds = da.to_dataset()
+        else:
+            new_ds[var] = da
+
+    if export_nc:
+        try:
+            new_ds.to_netcdf(os.path.join(out_pth, 'LISMonthly.nc'))
+        except IOError:
+            print('Folder not found.')
+
+    return new_ds

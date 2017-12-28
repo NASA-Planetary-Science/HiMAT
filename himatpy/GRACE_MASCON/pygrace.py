1a580c711f76f63e1a3b329de62fe3c4ee45ea3e
lsetiawan <landungs@uw.edu>
2017-12-28 14:06:24 -0800

    Add Masking function and new ipynb

--- himatpy/GRACE_MASCON/pygrace.py
+++ himatpy/GRACE_MASCON/pygrace.py
@@ -174,4 +174,35 @@ def trend_analysis(dec_year, series=None, optimization=False, pvalues = None):
     except:
         print("no pvalues!") 
         
+
+def build_mask(dsbbox, mascon_gdf, dacoords, serialize=False, datadir=None):
+    """
+    dsbbox: List or Tuple 
+        minx, miny, maxx, maxy
+    """
+    # Build a polygon around the extent of the LIS output so we can subset the GRACE data
+    coords = [(dsbbox[0],dsbbox[1]), (dsbbox[0],dsbbox[3]), (dsbbox[2],dsbbox[3]), (dsbbox[2], dsbbox[1])]
+    # Create a Shapely polygon from the coordinate-tuple list
+    lispoly = Polygon(coords)
+    df = pd.DataFrame.from_records([{'region_name': 'dataset'}])
+    geometry = [lispoly]
+    regiondf = gpd.GeoDataFrame(df, geometry=geometry, crs={'init': 'epsg4326'})
     
+    gpd_intersect = gpd.overlay(regiondf, mascon_gdf, how='intersection')
+    
+    # Create mask
+    numbers = gpd_intersect.index.values
+    names = gpd_intersect['mascon'].values
+    abbrevs = gpd_intersect['mascon'].values
+    m = regionmask.Regions_cls('region_mask',numbers,names,abbrevs,gpd_intersect.geometry.values)
+    m.name = 'mascon'
+    
+    gracemsk = m.mask(dacoords, lon_name = 'longitude', lat_name = 'latitude')
+    gracemsk.name = 'mask'
+    
+    if serialize:
+        if datadir:
+            gracemsk.to_netcdf(os.path.join(datadir, 'gracemsk.nc'))
+        else:
+            print('Need datadir to be specified.')
+    return gracemsk

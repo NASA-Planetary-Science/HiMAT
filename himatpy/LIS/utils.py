f2a2a18b8b5b13b490912560eef9f0c709d6e21f
aaarendt <arendta@uw.edu>
2019-04-22 14:05:11 -0700

    add option for single or multidimensional coordinates in the returned ds

--- himatpy/LIS/utils.py
+++ himatpy/LIS/utils.py
@@ -14,7 +14,7 @@ from dask.diagnostics import ProgressBar
 __author__ = ['Anthony Arendt', 'Landung Setiawan']
 
 
-def get_xr_dataset(datadir=None, fname=None, multiple_nc=False, files=[], **kwargs):
+def get_xr_dataset(datadir=None, fname=None, multiple_nc=False, twoDcoords=False, files=[], **kwargs):
     """
     Reads in output from the NASA Land Information System (LIS) model.
     Returns a "cleaned" xarray dataset. Users can read a single or multiple NetCDF file(s). 
@@ -22,6 +22,7 @@ def get_xr_dataset(datadir=None, fname=None, multiple_nc=False, files=[], **kwar
     :param datadir: path to directory containing the data, e.g. '/Users/lsetiawan/Downloads/200101/' or r'C:\work\datadrive\LIS\'
     :param fname: file name if only opening one NetCDF file
     :param multiple_nc: True if using to read multiple NetCDF Files
+    :param twoDcoords: True if you want multidimensional coordinates
      **kwargs
         Arbitrary keyword arguments related to xarray open_dataset or open_mfdataset.
     :return: xarray dataset
@@ -49,12 +50,16 @@ def get_xr_dataset(datadir=None, fname=None, multiple_nc=False, files=[], **kwar
     ny = ds.dims['north_south']
     x = np.arange(xmn, xmn + dx * nx, dx)
     y = np.arange(ymn, ymn + dy * ny, dy)
-    # create tile from x, y
-    data = np.tile(x, (ny, 1))
-    ds.coords['longitude'] = (('north_south', 'east_west'), data)
-    data = np.tile(y, (nx, 1))
-    ds.coords['latitude'] = (('north_south', 'east_west'), np.swapaxes(data, 0, 1))
-
+    if twoDcoords:
+        # create tile from x, y
+        data = np.tile(x, (ny, 1))
+        ds.coords['long'] = (('north_south', 'east_west'), data)
+        data = np.tile(y, (nx, 1))
+        ds.coords['lat'] = (('north_south', 'east_west'), np.swapaxes(data, 0, 1))
+    else:     
+        ds.coords['long'] = (('east_west'), x)
+        ds.coords['lat'] = (('north_south'), y)
+        
     return ds
 
 
